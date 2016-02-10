b6768db9aa9b1f4de28900b173ede4d68ad24ab1
didierClin <didier.c62@numericable.fr>
2016-02-10 15:40:47 +0100

    First commit tardif

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  mount_uploader :avatar, AvatarUploader
+  validates :name, presence: true
+  validates :avatar, presence: true
+  # validates :email, presence: true
+  has_many :posts
+end
