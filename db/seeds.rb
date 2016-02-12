0ac7f13c489f870b8e5eaf17c7596d2a888c6af9
didierClin <didier.c62@numericable.fr>
2016-02-12 22:37:07 +0100

    Add seed post(ok) et user avatar (ko)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-users = User.create([{name: 'paul', email: 'paul@beatles.fr', password: '123456789', avatar: 'favicon.png' }, {name: 'john', email: 'john@beatles.fr', password: '123456789', avatar: 'favicon.png' }, {name: 'ringo', email: 'ringo@beatles.fr', password: '123456789', avatar: 'favicon.png' }])
-#:name, :email, :password, :avatar
+
+users = [{name: 'paul', email: 'paul@beatles.fr', password: '123456789', avatar: 'kiwi_16.png' }]
+# ,
+#     {name: 'john', email: 'john@beatles.fr', password: '123456789', avatar: 'kiwi_16.png' },
+#     {name: 'ringo', email: 'ringo@beatles.fr', password: '123456789', avatar: 'kiwi_16.png' }]
+User.delete_all
+
+users.each do |u|
+  u.each do |name, email, password, avatar|
+  u = User.create(:name => name, :email => email, :password => password)
+  # u = User.create(:name => name, :email => email, :password => password, :avatar => File.open(Rails.root + "/public/images/#{avatar}"))
+  # pi.image.store!(File.open(File.join(Rails.root, 'test.jpg')))
+  # u.avatar = )
+  # User.create(x)
+end
+end
+
+posts = [{name: 'post 1', content: 'premier contenu', user_id: 5},
+    {name: 'post 2', content: 'second contenu ', user_id: 5},
+    {name: 'post 3', content: 'troisieme contenu', user_id: 5}]
+
+Post.delete_all
+posts.each do |x|
+  Post.create(x)
+end
